cad51d0f512e97c2408c56904e73d1a2c758fecc
Kevin Sony <kevinsony2003@gmail.com>
2026-03-10 22:04:30 -0500

    huge addition of exercises and notes for various problems

--- collect_compile_notes.py
+++ collect_compile_notes.py
@@ -9,7 +9,19 @@ out_dir_list = ["./algebraic_topology", "./differential_geometry"]
 
 assert(len(src_dir_list) == len(out_dir_list))
 
+# first clear out the old directories
+try:
+    for i in range(len(out_dir_list)):
+        # remove the directory and then create it again
+        shutil.rmtree(out_dir_list[i])
+        os.makedirs(out_dir_list[i])
+except Exception as e:
+    print(f'Error clearing directory: {e}')
+
+
 for i in range(len(src_dir_list)):
     for file in glob.glob(os.path.join(src_dir_list[i], "*.tex")):
         shutil.copy(file, out_dir_list[i])
-        print(type(file))
+        filename = os.path.basename(file)
+        subprocess.run(["pdflatex.exe", filename], cwd=out_dir_list[i])
+        print(filename)
